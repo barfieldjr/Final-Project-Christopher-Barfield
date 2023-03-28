b4fe39c58cf0116ed9809fd56daf4131baa10273
Christopher Barfield Jr <73442540+barfieldjr@users.noreply.github.com>
2023-03-27 20:36:33 -0400

    Model Repository Controller
    MRCs for invoice and fee.

---
+++ gamestore/src/main/java/com/company/gamestore/model/Invoice.java
@@ -0,0 +1,167 @@
+package com.company.gamestore.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "invoice")
+public class Invoice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "invoice_id")
+    private Integer invoiceId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "zipcode")
+    private String zipcode;
+
+    @Column(name = "item_type")
+    private String itemType;
+
+    @Column(name = "item_id")
+    private Integer itemId;
+
+    @Column(name = "unit_price")
+    private double unitPrice;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "subtotal")
+    private double subtotal;
+
+    @Column(name = "tax")
+    private double tax;
+
+    @Column(name = "processing_fee")
+    private double processingFee;
+
+    @Column(name = "total")
+    private double total;
+
+    public Invoice() {}
+
+    public Integer getInvoiceId() {
+        return invoiceId;
+    }
+
+    public void setInvoiceId(Integer invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public double getTax() {
+        return tax;
+    }
+
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+
+    public double getProcessingFee() {
+        return processingFee;
+    }
+
+    public void setProcessingFee(double processingFee) {
+        this.processingFee = processingFee;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+}
